744ef3eca4a81e4ce8f24be5eba354a0d3f59c55
azik25 <143266761+azik25@users.noreply.github.com>
2023-10-06 23:28:56 +0300

    Update issue-comments.cy.js

--- cypress/e2e/issue-comments.cy.js
+++ cypress/e2e/issue-comments.cy.js
@@ -68,4 +68,31 @@ describe('Issue comments creating, editing and deleting', () => {
             .find('[data-testid="issue-comment"]')
             .should('not.exist');
     });
+    it('Adding, updating and deleting comments', () => {
+        const comment = faker.lorem.words();
+        const editedComment = faker.lorem.words();
+
+        cy.get('[data-testid="modal:issue-details"]').within(() => {
+        cy.contains('Add a comment...').click();
+        cy.get('textarea[placeholder="Add a comment..."]').type(comment);
+        cy.contains('button', 'Save').click().should('not.exist');
+        cy.contains('Add a comment...').should('exist');
+        cy.get('[data-testid="issue-comment"]').should('contain', comment);
+        cy.get('[data-testid="issue-comment"]').first().contains('Edit').click().should('not.exist');
+        cy.get('textarea[placeholder="Add a comment..."]').should('contain', comment).clear().type(editedComment);
+        cy.contains('button', 'Save').click().should('not.exist');
+        cy.get('[data-testid="issue-comment"]').should('contain', 'Edit').and('contain', editedComment);
+        });
+
+        cy.get('[data-testid="issue-comment"]').should('have.length', 2).and('contain', editedComment)
+        cy.get('[data-testid="issue-comment"]').first().within(() => {
+        cy.contains('Delete').click()
+            });
+
+        cy.get('[data-testid="modal:confirm"]').should('exist').within(() => {
+        cy.contains('button', 'Delete comment').click().should('not.exist');
+        });
+        cy.get('[data-testid="modal:issue-details"]').find('[data-testid="issue-comment"]').
+        should('have.length', 1).and('not.contain', editedComment)
+    });
 });
