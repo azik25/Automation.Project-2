f367d2f68077e2d3a0b0281da47f5a6a74c8bb1b
azik25 <143266761+azik25@users.noreply.github.com>
2024-05-31 17:49:06 +0300

    Update issue-delete.cy.js

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -1,43 +1,41 @@
 describe('Issue deleting', () => {
-  beforeEach(() => {
-    cy.visit('/');
-    cy.url().should('eq', `${Cypress.env('baseUrl')}project`).then((url) => {
-      cy.visit(url + '/board');
-      cy.contains('This is an issue of type: Task.').click();
-    });
-  });
+    beforeEach(() => {
+        cy.visit('/');
+        cy.url().should('eq', `${Cypress.env('baseUrl')}project`).then((url) => {
+          cy.visit(url + '/board');
+        });
+        cy.contains('This is an issue of type: Task.').click();
+      });
+      
+  
     it('Should delete an issue', () => { 
       cy.get('[data-testid="modal:issue-details"]').should('be.visible');
       cy.get('[data-testid="icon:trash"]').click();
       cy.get('[data-testid="modal:confirm"]').should('be.visible').within(() => {
-      cy.contains('Delete issue').click();
-    });
+        cy.contains('Delete issue').click();
+      });
       
       cy.get('[data-testid="modal:confirm"]').should('not.exist');
-
-      cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
-      cy.get('[data-testid="list-issue"]').should('have.length', '3')
-
-      cy.contains('This is an issue of type: Task.').should('not.exist')
+  
+      cy.get('[data-testid="board-list:backlog"]').should('be.visible').and('have.length', 1).within(() => {
+        cy.get('[data-testid="list-issue"]').should('have.length', 3); // Corrected to use number instead of string
+        cy.contains('This is an issue of type: Task.').should('not.exist');
       });
-
-
-  });
-
-    it('Should cancel deleting the issue ', () => {
-      
+    });
+  
+    it('Should cancel deleting the issue', () => {
       cy.get('[data-testid="modal:issue-details"]').should('be.visible');
       cy.get('[data-testid="icon:trash"]').click();
       cy.get('[data-testid="modal:confirm"]').should('be.visible').within(() => {
-      cy.contains('Cancel').click();
-    });
-    
+        cy.contains('Cancel').click();
+      });
+      
       cy.get('[data-testid="modal:confirm"]').should('not.exist');
-
-      cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
-      cy.get('[data-testid="list-issue"]').should('have.length', '4')
-
-      cy.contains('This is an issue of type: Task.').should('be.visible');
+  
+      cy.get('[data-testid="board-list:backlog"]').should('be.visible').and('have.length', 1).within(() => {
+        cy.get('[data-testid="list-issue"]').should('have.length', 4); // Corrected to use number instead of string
+        cy.contains('This is an issue of type: Task.').should('be.visible');
+      });
     });
   });
-});
+  
