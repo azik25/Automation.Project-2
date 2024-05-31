77573cca45a6e73b0e4f09a5516a4495e0f7d155
azik25 <143266761+azik25@users.noreply.github.com>
2024-05-31 18:19:47 +0300

    Update issue-delete-in-pom.cy.js

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -1,25 +1,26 @@
-
 import IssueModal from "../../pages/IssueModal";
 
 describe('Issue delete', () => {
+  const issueTitle = 'This is an issue of type: Task.';
+  
   beforeEach(() => {
     cy.visit('/');
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    
-    cy.contains(issueTitle).click();
+      cy.visit(url);
+      cy.contains(issueTitle).click();
     });
   });
-const issueTitle = 'This is an issue of type: Task.';
 
   it('Should delete issue successfully', () => {
-    IssueModal.clickDeleteButton()
-    IssueModal.confirmDeletion()
-    IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle)
+    IssueModal.clickDeleteButton();
+    IssueModal.confirmDeletion();
+    IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle);
   });
+
   it('Should cancel deletion process successfully', () => {
-    IssueModal.clickDeleteButton()
-    IssueModal.cancelDeletion()
-    IssueModal.closeDetailModal()
-    IssueModal.ensureIssueIsVisibleOnBoard(issueTitle)
+    IssueModal.clickDeleteButton();
+    IssueModal.cancelDeletion();
+    IssueModal.closeDetailModal();
+    IssueModal.ensureIssueIsVisibleOnBoard(issueTitle);
   });
 });
